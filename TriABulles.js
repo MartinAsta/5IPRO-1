e425ff37fb3a4bdfd08267e83c83148f358bc659
Asta Martin <martin.asta@hotmail.com>
2022-12-06 18:30:08 +0100

    Ajout de l'algorithme de la fonction 'Tri à bulles'

--- TriABulles.js
+++ TriABulles.js
@@ -2,8 +2,8 @@ function bubbleSort(tableau) {
     for (let i = 0; i < tableau.length; i++) {
         for (let j = 0; j < tableau.length - 1; j++) {
             if (tableau[j] > tableau[j + 1]) {
-                let savedValue = tableau[j+1];
-                tableau[j + 1] = tableau[j]; 
+                let savedValue = tableau[j + 1];
+                tableau[j + 1] = tableau[j];
                 tableau[j] = savedValue;
             }
         }
