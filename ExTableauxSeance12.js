9a31503bf048a4a4ea79564dde4e5f4de367cb78
Asta Martin <martin.asta@hotmail.com>
2022-12-15 18:47:24 +0100

    Fini les exercices sur les tableaux de la séance 12.

--- ExTableauxSeance12.js
+++ ExTableauxSeance12.js
@@ -30,8 +30,8 @@ function avoidOddNumbers(array) {
  * Créez et parcourez un tableau d’entier du premier élément au dernier en évitant les index dont le modulo 4 est 0
  */
 function avoidModulo4(array) {
-    for (let i = 0; i < array.length; i++){
-        if(!isModulo4(i)){
+    for (let i = 0; i < array.length; i++) {
+        if (!isModulo4(i)) {
             console.log(array[i]);
         }
     }
@@ -39,4 +39,40 @@ function avoidModulo4(array) {
 
 function isModulo4(number) {
     return number % 4 == 0;
+}
+
+/**Exercice 5
+ * Créez et parcourez un tableau en sommant les éléments à leur place
+ */
+function progressiveSum(array) {
+    let newArray = [];
+    let lastSum = 0;
+    for (let i = 0; i < array.length; i++) {
+        newArray.push(lastSum + array[i]);
+        lastSum += array[i];
+    }
+    return newArray;
+}
+
+/**Exercice 6
+ * Pareil que le précédent, sauf que vous avez un tableau de tableaux, une fois la somme faites triez les par ordre décroissant
+ */
+function progressiveSumAndDescSort(array) {
+    let newArray = [];
+    for (let i = 0; i < array.length; i++) {
+        newArray.push(progressiveSum(array[i]));
+    }
+    sortMatrixesDesc(newArray);
+    return newArray;
+}
+
+function sortMatrixesDesc(array) {
+    let savedValue = [];
+    for (let i = 1; i < array.length; i++) {
+        if (array[i][0] > array[i - 1][0]) {
+            savedValue = array[i];
+            array[i] = array[i - 1];
+            array[i - 1] = savedValue;
+        }
+    }
 }
